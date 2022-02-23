4cd02544374e55aba12a253fe0c7b55560f190fd
nplappert <nico.plappert@gmail.com>
2022-02-23 18:46:25 +0100

    Removed dist folder

--- dist/src/interfaces/token-payload.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Cookies = void 0;
-var Cookies;
-(function (Cookies) {
-    Cookies["AccessToken"] = "access";
-    Cookies["RefreshToken"] = "refresh";
-})(Cookies = exports.Cookies || (exports.Cookies = {}));
-//# sourceMappingURL=token-payload.js.map
--- dist/src/services/user.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "../models/user.model";
-declare const _default: {
-    createUser(email: string, password: string): Promise<string>;
-    getUserById(userId: string): Promise<User>;
-    getUserByEmail(email: string): Promise<User>;
-    increaseTokenVersion(userId: string): Promise<string>;
-    deleteUser(userId: string): Promise<void>;
-};
-export default _default;
